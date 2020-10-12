a82e1d9ba502ff57fdd41fa683763240b0e5be39
sdudin <serrgeynd@rambler.ru>
2020-10-13 00:10:48 +0300

    Убрал Main. Добавил модификаторы доступа.

--- src/Circle.java
+++ src/Circle.java
@@ -1,19 +1,14 @@
+import java.util.Scanner;
+
 public class Circle<PI> implements Shape {
-    double r = 10;
-    double s;
+    public double r = 10;
+    Scanner scanner = new Scanner(System.in);
 
     public double square() {
+        System.out.println("Введите радиус: ");
+        r = scanner.nextDouble();
         double s = Math.PI * r * r;
         return s;
     }
 
-    public static void main(String[] args) {
-        Shape[] shape = new Shape[3];
-        shape[0] = new Triangle();
-        shape[1] = new Circle();
-        shape[2] = new Rectangle();
-        for (Shape sh : shape) {
-            System.out.println("Площадь фигуры: " + sh.square());
-        }
-    }
 }
