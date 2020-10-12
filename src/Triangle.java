7ab54eaa092782dbdc64e87591af68b02012a5ce
sdudin <serrgeynd@rambler.ru>
2020-10-13 00:12:48 +0300

    Убрал Main. Добавил модификаторы доступа.

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,21 +1,18 @@
-public class Triangle implements Shape {
-    double a = 3;
-    double b = 4;
+import java.util.Scanner;
 
+public class Triangle implements Shape {
+
+    Scanner scanner = new Scanner(System.in);
+    double a;
+    double h;
 
     public double square() {
-        double s = a * b / 2;
-
+        System.out.println("Введите высоту и прилегающую сторону: ");
+        a = scanner.nextDouble();
+        h = scanner.nextDouble();
+        double s = a * h / 2;
         return s;
     }
 
-    public static void main(String[] args) {
-        Shape[] shape = new Shape[3];
-        shape[0] = new Triangle();
-        shape[1] = new Circle();
-        shape[2] = new Rectangle();
-        for (Shape sh : shape) System.out.println("Площадь фигуры: " + sh.square());
-
-    }
 }
 
